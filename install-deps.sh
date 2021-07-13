9fe507d2477c9fc916a3e2f795003a7261ee9497
vietnguyen3 <viet.nguyen3@tiki.vn>
2021-07-13 15:34:00 +0700

    update

--- install-deps.sh
+++ install-deps.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-export RDK_PREFIX=/usr/local
+
 sudo wget https://github.com/edenhill/librdkafka/archive/v1.7.0.tar.gz  -O - | sudo tar -xz
 cd librdkafka-1.7.0/
-./configure --prefix=$RDK_PREFIX
-sudo make 
+sudo ./configure --install-deps
+sudo make
 sudo make install
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$RDK_PREFIX/lib
